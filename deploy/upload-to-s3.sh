9f21db8b136650376dc6d3cebd6323cf3a3f0f48
RomanKoronovskyi <102824796+RomanKoronovskyi@users.noreply.github.com>
2025-06-13 11:55:41 +0300

    Create upload-to-s3.sh

---
+++ deploy/upload-to-s3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+WarFile = "target/vprofile-v2.war"
+S3Bucket = "web-artifacts-tomcat"
+
+mvn clean install || {echo "mvn error"; exit 1;}
+aws s3 cp "WarFile" "s3://S3Bucket/" || {echo "aws error"; exit 1;}
