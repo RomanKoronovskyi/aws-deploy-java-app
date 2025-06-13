2e5c94a0234fd2fe09fef2ace54e40d5e7435b48
RomanKoronovskyi <102824796+RomanKoronovskyi@users.noreply.github.com>
2025-06-13 11:54:57 +0300

    Create install-to-ec2.sh

---
+++ deploy/install-to-ec2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+WarFile = "vprofile-v2.war"
+S3Bucket = "web-artifacts-tomcat"
+TomcatWebApps = "/var/lib/tomcat10/webapps"
+
+snap install aws-cli --classic
+aws s3 cp "s3://S3Bucket/WarFile" /tmp/ || {echo "error aws"; exit 1;}
+systemctl daemon-reload
+systemctl stop tomcat10
+
+rm -rf "$TomcatWebApps/ROOT"
+cp "/tmp/WarFile" "TomcatWebApps/ROOT.war" || {echo "error cp WarFile"; exit 1;}
+
+systemctl start tomcat10
