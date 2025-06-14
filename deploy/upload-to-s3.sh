c21b3953b42e7059ee90354c57351724c415589f
RomanKoronovskyi <102824796+RomanKoronovskyi@users.noreply.github.com>
2025-06-14 21:28:28 +0300

    Update upload-to-s3.sh

--- deploy/upload-to-s3.sh
+++ deploy/upload-to-s3.sh
@@ -3,5 +3,5 @@
 WarFile="target/vprofile-v2.war"
 S3Bucket="web-artifacts-tomcat"
 
-mvn clean install || {echo "mvn error"; exit 1;}
-aws s3 cp "WarFile" "s3://S3Bucket/" || {echo "aws error"; exit 1;}
+mvn clean install || { echo "mvn error"; exit 1; }
+aws s3 cp "$WarFile" "s3://$S3Bucket/" || { echo "aws error"; exit 1; }
