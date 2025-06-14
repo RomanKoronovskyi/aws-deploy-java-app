6949711ea141a0c3546bb9a4d43a346e68986bc0
RomanKoronovskyi <102824796+RomanKoronovskyi@users.noreply.github.com>
2025-06-14 21:22:52 +0300

    Update install-to-ec2.sh

--- deploy/install-to-ec2.sh
+++ deploy/install-to-ec2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-WarFile = "vprofile-v2.war"
-S3Bucket = "web-artifacts-tomcat"
-TomcatWebApps = "/var/lib/tomcat10/webapps"
+WarFile="vprofile-v2.war"
+S3Bucket="web-artifacts-tomcat"
+TomcatWebApps="/var/lib/tomcat10/webapps"
 
 snap install aws-cli --classic
 aws s3 cp "s3://S3Bucket/WarFile" /tmp/ || {echo "error aws"; exit 1;}
