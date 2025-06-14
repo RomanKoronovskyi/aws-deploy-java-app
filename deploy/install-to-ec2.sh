07ce01d5c81f195db335b7f5bd51f0cb35015d2d
RomanKoronovskyi <102824796+RomanKoronovskyi@users.noreply.github.com>
2025-06-14 21:26:47 +0300

    Update install-to-ec2.sh

--- deploy/install-to-ec2.sh
+++ deploy/install-to-ec2.sh
@@ -5,11 +5,15 @@ S3Bucket="web-artifacts-tomcat"
 TomcatWebApps="/var/lib/tomcat10/webapps"
 
 snap install aws-cli --classic
-aws s3 cp "s3://S3Bucket/WarFile" /tmp/ || {echo "error aws"; exit 1;}
+
+aws s3 cp "s3://$S3Bucket/$WarFile" /tmp/ || { echo "error aws"; exit 1; }
 systemctl daemon-reload
 systemctl stop tomcat10
 
 rm -rf "$TomcatWebApps/ROOT"
-cp "/tmp/WarFile" "TomcatWebApps/ROOT.war" || {echo "error cp WarFile"; exit 1;}
+cp "/tmp/$WarFile" "$TomcatWebApps/ROOT.war" || { echo "error cp WarFile"; exit 1; }
 
 systemctl start tomcat10
+
+exit 0
+
